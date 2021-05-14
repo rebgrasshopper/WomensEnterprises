15c6f94ebc265186bb8578cb3c3dfe0480dedc5c
Plover Brown <56779568+rebgrasshopper@users.noreply.github.com>
2021-05-14 13:54:53 -0700

    carousel working

--- womensenterprises/src/components/carousel/carousel.js
+++ womensenterprises/src/components/carousel/carousel.js
@@ -1,18 +1,40 @@
-export default function Carousel({photos}){
+import React, { Component } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+export default function CarouselComponent({photos}){
 
     return (
-        <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
-            <div className="carousel-inner">
+        // <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+        //     <div className="carousel-inner">
                 
-                {photos.map(({photo, alt}) => {
-                    return (
-                    <div className="carousel-item active" key={alt}>
-                        <img src={photo} className="d-block w-100" alt={alt}></img>
-                    </div>
-                    )
-                })}
+        //         {photos.map(({photo, alt}) => {
+        //             return (
+        //             <div className="carousel-item active" key={alt}>
+        //                 <img src={photo} className="d-block w-100" alt={alt}></img>
+        //             </div>
+        //             )
+        //         })}
 
-            </div>
-        </div>
+        //     </div>
+        // </div>
+
+
+
+
+
+                    <div className='container-fluid' >
+                         <Carousel>
+                            {photos.map(({photo, alt, caption}) => {
+                                return (
+                                    <Carousel.Item style={{'height':"600px", 'objectFit':"cover"}} >
+                                        <img style={{'height':"600px", 'objectFit':"cover"}} className="d-block w-100" src={photo} alt={alt}></img>
+                                        <Carousel.Caption>
+                                            <h3>{caption}</h3>
+                                        </Carousel.Caption>
+                                    </Carousel.Item  >  
+                                )
+                            })}
+                             
+                        </Carousel>
+                    </div> 
         )
 }
--- womensenterprises/src/pages/home/home.js
+++ womensenterprises/src/pages/home/home.js
@@ -1,14 +1,15 @@
-import Carousel from "../../components/carousel/carousel";
+import CarouselComponent from "../../components/carousel/carousel";
 import pic1 from '../../images/Landing/pexels-andrea-piacquadio-cropped.jpg';
 import pic2 from '../../images/Landing/pexels-anna-tarazevich-cropped.jpg';
+import pic3 from '../../images/Landing/pexels-cottonbro-cropped.jpg';
 
 export default function Home(){
 
-    let photos = [{photo: pic1, alt:"Photo by Andrea Piacquadio, from Pexels"}, {photo: pic2, alt:"Photo by Anna Tarazevich, from Pexels"}];
+    let photos = [{photo: pic2, alt:"Photo by Andrea Piacquadio, from Pexels", caption:"Let's support women in our communities"}, {photo: pic1, alt:"Photo by Anna Tarazevich, from Pexels", caption:"Let's bring to life ideas from minority voices"}, {photo: pic3, alt:"Photo by Cottonbro, from Pexels", caption:"WE supports women lead organizations"}];
 
     return (
         <div>
-            <Carousel photos={photos} />
+            <CarouselComponent photos={photos} />
         </div>
         )
 }
