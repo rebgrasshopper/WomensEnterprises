65ed923ef32051e21aeb027506c92c2cbceb051c
Plover Brown <56779568+rebgrasshopper@users.noreply.github.com>
2021-06-11 21:41:52 -0700

    added icons to front page

--- womensenterprises/src/components/displayHome/displayHome.js
+++ womensenterprises/src/components/displayHome/displayHome.js
@@ -1,26 +1,43 @@
 import './displayHome.css';
+import Faces1 from '../../images/icons/WomensFaces3.png';
+import Faces2 from '../../images/icons/WomensFaces2.png';
+import Vert1 from "../../images/icons/VertFaces1.png";
+import Vert2 from "../../images/icons/VertFaces2.png";
+import Vert3 from "../../images/icons/VertFaces3.png";
 
 export default function DisplayHome({homeData}) {
     if (homeData) {
         return (
             <div id="displayHome">
                 <div className="textDiv">
-                    <h1 className="textH1">{homeData.recentMessageTitle}</h1>
-                    {homeData.recentMessage.split("\n").map((element, index) => {
-                        return <div className = "textContent" key={index}>{element}</div>
-                    })}
+                    <div className="textDivContent">
+                        <h1 className="textH1">{homeData.recentMessageTitle}</h1>
+                        <div className="textDivContentWords">
+                            {homeData.recentMessage.split("\n").map((element, index) => {
+                                return <div className = "textContent" key={index}>{element}</div>
+                            })}
+                        </div>
+                    </div>
                 </div>
+                    {/* <img className="faceImages" src={Faces1} alt="Women's Faces"></img> */}
                  <div className="textDiv">
-                    <h1 className="textH1">{homeData.missionTitle}</h1>
-                    {homeData.mission.split("\n").map((element, index) => {
-                        return <div className = "textContent" key={index}>{element}</div>
-                    })}
+                    <img className="verticalFaces" src={Vert1} alt="Women's Faces"></img>
+                     <div className="textDivContent">
+                        <h1 className="textH1">{homeData.missionTitle}</h1>
+                        {homeData.mission.split("\n").map((element, index) => {
+                            return <div className = "textContent" key={index}>{element}</div>
+                        })}
+                     </div>
+                        <img className="verticalFaces" src={Vert2} alt="Women's Faces"></img>
                 </div>
+                {/* <img className="faceImages" src={Faces2} alt="Women's Faces"></img> */}
                 <div className="textDiv">
-                    <h1 className="textH1">{homeData.resultsTitle}</h1>
-                    {homeData.results.split("\n").map((element, index) => {
-                        return <div className = "textContent" key={index}>{element}</div>
-                    })}
+                    <div className="textDivContent">
+                        <h1 className="textH1">{homeData.resultsTitle}</h1>
+                        {homeData.results.split("\n").map((element, index) => {
+                            return <div className = "textContent" key={index}>{element}</div>
+                        })}
+                    </div>
                 </div>
             </div>
             )
