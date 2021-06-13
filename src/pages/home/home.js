60adcc7cdbb04afe9face03161347cd299a57d29
Plover Brown <56779568+rebgrasshopper@users.noreply.github.com>
2021-06-13 09:05:40 -0700

    change to routes, trying to add backend services - initiated graphql

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -31,7 +31,7 @@ export default function Home(){
 
     const fetchData = async () => {
         axios
-            .get('/api/home')
+            .get('http://localhost:4001/api/home')
             .then(response => {
                 setHomeData(response.data[0])
             })
